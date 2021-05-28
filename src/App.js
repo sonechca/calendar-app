e0b23248afd0957cacb33f85a9d1e0a1c2f384af
Mintae <mintae.khim@gmail.com>
2021-05-28 02:08:43 -0600

    Create the calendar with option which user can select week when user select a day

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Helmet from 'react-helmet';
+import moment from 'moment';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getWeekDays(weekStart) {
+  const days = [weekStart];
+  days.pop(0)
+  for (let i = 1; i < 8; i ++) {
+    days.push(
+      moment(weekStart)
+        .add(i, 'days')
+        .toDate()
+    );
+  }
+  for(let i = 0; i < 7; i ++){
+    console.log(days[i])
+  }
+  return days;
+}
+
+function getWeekRange(date) {
+  return {
+    from: moment(date)
+      .startOf('week')
+      .toDate(),
+    to: moment(date)
+      .endOf('week')
+      .toDate(),
+  };
+}
+
+class App extends React.Component{
+  state = {
+    hoverRange: undefined,
+    selectedDays: [],
+  };
+
+  handleDayChange = date => {
+    this.setState({
+      selectedDays: getWeekDays(getWeekRange(date).from),
+    });
+  };
+
+  handleDayEnter = date => {
+    this.setState({
+      hoverRange: getWeekRange(date),
+    });
+  };
+
+  handleDayLeave = () => {
+    this.setState({
+      hoverRange: undefined,
+    });
+  };
+
+  handleWeekClick = (weekNumber, days, e) => {
+    this.setState({
+      selectedDays: days,
+    });
+  };
+
+  render() {
+    const { hoverRange, selectedDays } = this.state;
+
+    const daysAreSelected = selectedDays.length > 0;
+
+    const modifiers = {
+      hoverRange,
+      selectedRange: daysAreSelected && {
+        from: selectedDays[0],
+        to: selectedDays[6],
+      },
+      hoverRangeStart: hoverRange && hoverRange.from,
+      hoverRangeEnd: hoverRange && hoverRange.to,
+      selectedRangeStart: daysAreSelected && selectedDays[0],
+      selectedRangeEnd: daysAreSelected && selectedDays[6],
+    };
+
+    return (
+      <div className="SelectedWeekExample">
+        <DayPicker
+          selectedDays={selectedDays}
+          showWeekNumbers
+          showOutsideDays
+          modifiers={modifiers}
+          onDayClick={this.handleDayChange}
+          onDayMouseEnter={this.handleDayEnter}
+          onDayMouseLeave={this.handleDayLeave}
+          onWeekClick={this.handleWeekClick}
+        />
+        {selectedDays.length === 7 && (
+          <div>
+            {moment(selectedDays[0]).format('LL')} –{' '}
+            {moment(selectedDays[6]).format('LL')}
+          </div>
+          
+        )}
+
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
